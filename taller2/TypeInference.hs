e0bedcb8df901d0e23fa90b179dc4c1f3f48bd88
JGodoy <j.godoy277@gmail.com>
2021-04-29 21:14:51 -0300

    compila

--- taller2/TypeInference.hs
+++ taller2/TypeInference.hs
@@ -1,6 +1,8 @@
-module TypeInference (TypingJudgment, Result (..), inferType, inferNormal, normalize) where
+module TypeInference (TypingJudgment, Result(..), inferType, inferNormal, normalize)
 
-import Data.List (intersect, nub, sort, union)
+where
+
+import Data.List(intersect, union, nub, sort)
 import Exp
 import Type
 import Unification
@@ -10,18 +12,18 @@ import Unification
 ------------
 data Result a = OK a | Error String
 
+
 --------------------
 -- Type Inference --
 --------------------
 type TypingJudgment = (Context, AnnotExp, Type)
 
 typeVarsT :: Type -> [Int]
-typeVarsT = foldType (: []) [] [] union
+typeVarsT = foldType (:[]) [] [] union
 
 typeVarsE :: Exp Type -> [Int]
-typeVarsE = foldExp (const []) [] id id id [] [] (\r1 r2 r3 -> nub (r1 ++ r2 ++ r3)) (const setAdd) union
-  where
-    setAdd t r = union (typeVarsT t) r
+typeVarsE = foldExp (const []) [] id id id [] [] (\r1 r2 r3 ->nub(r1++r2++r3)) (const setAdd) union
+  where setAdd t r = union (typeVarsT t) r
 
 typeVarsC :: Context -> [Int]
 typeVarsC c = nub (concatMap (typeVarsT . evalC c) (domainC c))
@@ -30,69 +32,66 @@ typeVars :: TypingJudgment -> [Int]
 typeVars (c, e, t) = sort $ union (typeVarsC c) (union (typeVarsE e) (typeVarsT t))
 
 normalization :: [Int] -> [Subst]
-normalization ns = foldr (\n rec (y : ys) -> extendS n (TVar y) emptySubst : (rec ys)) (const []) ns [0 ..]
+normalization ns = foldr (\n rec (y:ys) -> extendS n (TVar  y) emptySubst : (rec ys)) (const []) ns [0..]
 
 normalize :: TypingJudgment -> TypingJudgment
-normalize j@(c, e, t) = let ss = normalization $ typeVars j in foldl (\(rc, re, rt) s -> (s <.> rc, s <.> re, s <.> rt)) j ss
-
+normalize j@(c, e, t) = let ss = normalization $ typeVars j in foldl (\(rc, re, rt) s ->(s <.> rc, s <.> re, s <.> rt)) j ss
+  
 inferType :: PlainExp -> Result TypingJudgment
 inferType e = case infer' e 0 of
-  OK (_, tj) -> OK tj
-  Error s -> Error s
-
+    OK (_, tj) -> OK tj
+    Error s -> Error s
+    
 inferNormal :: PlainExp -> Result TypingJudgment
 inferNormal e = case infer' e 0 of
-  OK (_, tj) -> OK $ normalize tj
-  Error s -> Error s
+    OK (_, tj) -> OK $ normalize tj
+    Error s -> Error s
+
 
 infer' :: PlainExp -> Int -> Result (Int, TypingJudgment)
-infer' (ZeroExp) n = OK (n, (emptyContext, ZeroExp, TNat))
-infer' (VarExp x) n = OK (n + 1, (extendC emptyContext x (TVar n), VarExp x, TVar n))
-infer' (SuccExp e) n = case infer' e n of
-  OK (n', (c', e', t')) ->
-    case mgu [(t', TNat)] of
-      UOK subst ->
-        OK
-          ( n',
-            ( subst <.> c',
-              subst <.> SuccExp e',
-              TNat
-            )
-          )
-      UError u1 u2 -> uError u1 u2
-  res@(Error _) -> res
+
+infer' (SuccExp e)    n = case infer' e n of
+                            OK (n', (c', e', t')) ->
+                              case mgu [(t', TNat)] of
+                                UOK subst -> OK (n',
+                                                    (
+                                                     subst <.> c',
+                                                     subst <.> SuccExp e',
+                                                     TNat
+                                                    )
+                                                )
+                                UError u1 u2 -> uError u1 u2
+                            res@(Error _) -> res
 
 -- COMPLETAR DESDE AQUI
--- intersect l1 l2 = filter (\e -> elem e l2) l1
-intersect c1 c2 = filter (\t -> elem t c2) c1
 
-infer' (AppExp u v) n =
-    case infer' u n of
-      	OK (n', (c', u' , t') ) ->
-			case infer' v n' of
-				OK (n'', (c'', v' , t'') ) ->
-					case mgu [ (t', TFun t'' (TVar (n'' + 1) ))] ++ (intersect (c')(c'')) of
-						UOK subst -> OK(n'' + 1,
-										(
-											subst <.> joinC [c', c''],
-											subst <.> (AppExp u' v'),
-											subst <.> TVar (n'' + 1)
-										))
-						UError u1 u2 -> uError u1 u2
-				res2@(Error _) -> res2
-		res1@(Error _) -> res1
+infer' ZeroExp                n = OK (n, (emptyContext, ZeroExp, TNat))
+infer' (VarExp x)             n = OK (n+1, (extendC emptyContext x (TVar n) , VarExp x, TVar n))
+infer' (AppExp u v)           n = case infer' u n of
+									  OK (n', (c', u' , t')) -> 
+										case infer' v n' of
+										  OK (n'', (c'', v' , t'')) ->
+											case mgu [(t', (TFun t'' (TVar (n'' + 1) )))] of
+												UOK subst -> OK ((n'' + 1),
+																	(
+																		subst <.> joinC [c', c''],--c',
+																		subst <.> AppExp u' v',
+																		subst <.> TVar (n'' + 1)
+																	)
+																)
+												UError u1 u2 -> uError u1 u2
+										  res1@(Error _) -> res1
+									  res2@(Error _) -> res2
 
+--infer' (LamExp x _ e)         n = undefined
 
--- para s en la interseccion
--- verificar que eval (s, c') == eval(s, c'')
--- infer' (LamExp x _ e) n = undefined
 -- OPCIONALES
 
--- infer' (PredExp e) n = undefined
--- infer' (IsZeroExp e) n = undefined
--- infer' TrueExp n = undefined
--- infer' FalseExp n = undefined
--- infer' (IfExp u v w) n = undefined
+infer' (PredExp e)            n = undefined
+infer' (IsZeroExp e)          n = undefined
+infer' TrueExp                n = undefined
+infer' FalseExp               n = undefined
+infer' (IfExp u v w)          n = undefined
 
 --------------------------------
 -- YAPA: Error de unificacion --
